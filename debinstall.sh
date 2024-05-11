d15c6157755d158e79357d814c818f6a82363840
rockyou-dillow <154830044+rockyou-dillow@users.noreply.github.com>
2024-05-10 21:48:00 -0400

    Add files via upload

---
+++ debinstall.sh
@@ -0,0 +1,84 @@
+# debian installation
+# created by: william dillow
+
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+
+# system updates
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
+
+# Making .config and Moving config files and background to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Pictures/backgrounds
+cp -R dotconfig/* /home/$username/.config/
+cp bg.jpg /home/$username/Pictures/backgrounds/
+mv user-dirs.dirs /home/$username/.config
+chown -R $username:$username /home/$username
+
+
+# Install nala
+apt install nala -y
+
+# Installing programs
+nala install neofetch kitty mangohud lightdm vlc flameshot git feh lxappearance gcc-mingw-w64-x86-64 htop wireshark nmap hashcat hydra gobuster hexedit steghide device-tree-compiler fzf ripgrep fonts-terminus rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pipewire wireplumber pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev zoxide xdg-utils -y
+
+flatpak install flathub com.obsproject.Studio org.kde.kdenlive com.discordapp.Discord org.gimp.GIMP im.riot.Riot com.github.micahflee.torbrowser-launcher
+
+# Set the font to Terminus Fonts
+setfont /usr/share/consolefonts/Uni3-TerminusBold28x14.psf.gz
+
+# Clear the screen
+clear
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Installing fonts
+cd $builddir 
+nala install fonts-font-awesome -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+# Enable graphical login and change target from CLI to GUI
+systemctl enable lightdm
+systemctl set-default graphical.target
+
+# Enable wireplumber audio service
+sudo -u $username systemctl --user enable wireplumber.service
+
+# shodan
+pip install -U --user shodan
+sudo adduser $USER wireshark
+sudo chmod +x /usr/bin/dumpcap
+
+# qemu virtualization
+sudo apt install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+sudo virsh net-start default
+sudo virsh net-autostart default
+sudo usermod -aG libvirt $USER
+sudo usermod -aG libvirt-qemu $USER
+sudo usermod -aG kvm $USER
+sudo usermod -aG input $USER
+sudo usermod -aG disk $USER
